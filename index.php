c70c643dd2ff729cefffbdedf1b3efe0d47202b9
Zack Dithilath <dithilath.aphixath@greenriver.edu>
2023-02-03 05:13:27 -0500

    reroute more pages and added more forms to each of the html pages within views

--- index.php
+++ index.php
@@ -22,31 +22,65 @@ $f3->route('GET /', function() {
 });
 
 // info.html route (328/job-application/info.html)
-$f3->route('GET /info', function () {
-    //instantiate a view
-    $view = new Template(); /// template is a fat free class
-    echo $view->render("views/info.html"); // render method, return text on template
-});
-
-// info route -> views/info.html
-$f3->route('GET|POST /process_1', function ($f3) {
+$f3->route('GET|POST /info', function ($f3) {
 
     // if the form has been posted
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         var_dump($_POST); // For development process
 
         // move data from POST array to SESSION array
-        $_SESSION['food'] = $_POST['food'];
-        $_SESSION['meal'] = $_POST['meal'];
+        $_SESSION['fname'] = $_POST['fname'];
+        $_SESSION['lname'] = $_POST['lname'];
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['state'] = $_POST['state'];
+
 
         // redirect to summary page
         $f3->reroute('experience');
     }
-
-
     //instantiate a view
     $view = new Template(); /// template is a fat free class
     echo $view->render("views/info.html"); // render method, return text on template
+});
+
+//reroute from info to views/experience.html
+$f3->route('GET|POST /experience', function ($f3) {
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var dump for development
+        var_dump($_POST);
+
+        // variables
+        $_SESSION['bio'] = $_POST['bio'];
+        $_SESSION['link'] = $_POST['link'];
+        $_SESSION['years'] = $_POST['years'];
+        $_SESSION['relocate'] = $_POST['relocate'];
+
+        // direct to mailing
+        $f3->reroute('mailing');
+    }
+    // instantiate a view
+    $view = new Template();
+    echo $view->render("views/experience.html");
+});
+
+//reroute from experience to views/mailing.html
+$f3->route('GET|POST /mailing', function ($f3) {
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var dump for development
+        var_dump($_POST);
+
+        // variables
+        $_SESSION['bio'] = $_POST['bio'];
+        $_SESSION['link'] = $_POST['link'];
+        $_SESSION['years'] = $_POST['years'];
+        $_SESSION['relocate'] = $_POST['relocate'];
+
+        // direct to mailing
+        $f3->reroute('summary');
+    }
+    // instantiate a view
+    $view = new Template();
+    echo $view->render("views/mailing.html");
 });
 
 //run fat free
