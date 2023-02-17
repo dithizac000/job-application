2021d9caa508a0e029fa2ec38510ac5d1295cdec
Zack Dithilath <dithilath.aphixath@greenriver.edu>
2023-02-17 03:24:42 -0500

    add optional validation within model folder

---
+++ model/optional-valid.php
@@ -0,0 +1,11 @@
+<?php
+
+//checks each selected jobs checkbox selection against a list of valid options.
+function validSelectionsJob() {
+
+}
+
+// checks each selected verticals checkbox selection against a list of valid options
+function validSelectionsVerticals() {
+
+}
--- model/validate.php
+++ model/validate.php
@@ -1,6 +1,6 @@
 <?php
 
-// ) checks to see that a string is all alphabetic.
+// checks to see that a string is all alphabetic.
 function validName() {
 
 }
@@ -10,14 +10,14 @@ function validGithub() {
 
 }
 
-// checks to see that a string is a valid “value” property.
-function validExperience() {
-
+// checks to see that a phone number is valid (you can decide what constitutes a valid phone number, just make sure to check that there are numeric values entered).
+function validPhone() {
 
 }
 
-// checks to see that a phone number is valid (you can decide what constitutes a valid phone number, just make sure to check that there are numeric values entered).
-function validPhone() {
+// checks to see that a string is a valid “value” property.
+function validExperience() {
+
 
 }
 
@@ -26,12 +26,4 @@ function validEmail() {
 
 }
 
-//checks each selected jobs checkbox selection against a list of valid options.
-function validSelectionsJob() {
 
-}
-
-// checks each selected verticals checkbox selection against a list of valid options
-function validSelectionsVerticals() {
-
-}
