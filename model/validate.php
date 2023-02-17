f9d5c181368b2667f3898661ba2b5f59691224be
Zack Dithilath <dithilath.aphixath@greenriver.edu>
2023-02-17 02:53:58 -0500

    add model folder for php functions

---
+++ model/validate.php
@@ -0,0 +1,37 @@
+<?php
+
+// ) checks to see that a string is all alphabetic.
+function validName() {
+
+}
+
+// checks to see that a string is a valid github url.
+function validGithub() {
+
+}
+
+// checks to see that a string is a valid “value” property.
+function validExperience() {
+
+
+}
+
+// checks to see that a phone number is valid (you can decide what constitutes a valid phone number, just make sure to check that there are numeric values entered).
+function validPhone() {
+
+}
+
+//checks to see that an email address is valid
+function validEmail() {
+
+}
+
+//checks each selected jobs checkbox selection against a list of valid options.
+function validSelectionsJob() {
+
+}
+
+// checks each selected verticals checkbox selection against a list of valid options
+function validSelectionsVerticals() {
+
+}
