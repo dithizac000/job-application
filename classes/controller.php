4ab5933dcf7d0c53ff2e2d41a6e324db99aca519
Zack Dithilath <dithilath.aphixath@greenriver.edu>
2023-03-05 05:59:40 -0500

    update controller

--- classes/controller.php
+++ classes/controller.php
@@ -19,20 +19,144 @@ class Control
 
     function info()
     {
+
+        // if the form has been posted
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // instantiate new app
+            $newApp = new Application();
+
+            /// set state
+            $newApp->setState($_POST['state']);
+
+            //trim post names
+            $fname = trim($_POST['fname']);
+            $lname = trim($_POST['lname']);
+            if(Validation::validName($fname) && Validation::validName($lname)) {
+                // set app with post when validated
+                $newApp->setfname($fname);
+                $newApp->setlname($lname);
+
+            } else {
+                $this->_f3->set('errors["name"]',
+                    'Name must have at least 2 characters');
+            }
+
+            // email validation
+            $email = trim($_POST['email']);
+            if(Validation::validEmail($email)) {
+                $newApp->setEmail($email);
+            }
+            else $this->_f3->set('errors["email"]',
+                'Email must follow format email@example.com');
+
+
+            // phone validation
+            $phone = trim($_POST['phone']);
+            if(Validation::validPhone($phone))
+            {
+                $newApp->setPhone($phone);
+            }
+            else $this->_f3->set('errors["phone"]',
+                'Phone number must have at least 10 digits');
+
+
+            // checkbox for mailing
+            $mailbox = $_POST['mailingBox'];
+            if (isset($_POST['mailingBox'])){
+                $newApp->setMail($mailbox);
+            }
+
+            // if no errors, then reroute
+            if(empty($this->_f3->get('errors'))) {
+                //mailing session
+                $_SESSION['newApp'] = $newApp;
+                $this->_f3->reroute('experience');
+            }
+        }
+        //instantiate a view
+        $view = new Template(); /// template is a fat free class
+        echo $view->render("views/info.html"); // render method, return text on template
     }
 
     function expereince()
     {
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            //set and session variables
+            $_SESSION['newApp']->setBio($_POST['bio']);
 
+            $_SESSION['newApp']->setRelocate($_POST['relocate']);
+
+            //validate years selection
+            $years = $_POST['years'];
+            if(Validation::validYears($years)) {
+                $_SESSION['newApp']->setExp($years);
+            }
+            else $this->_f3->set('errors["years"]',
+                'Must Select Years');
+
+            //validate link git hub
+            $link = trim($_POST['link']);
+            if(Validation::validGithub($link)) {
+                $_SESSION['newApp']->setGitHub($link);
+            }
+            else $this->_f3->set('errors["link"]',
+                'Valid URL Format: http://www.example.com');
+
+            //Redirect to mailing page
+            //if there are no errors
+            if (empty($this->_f3->get('errors')) && ($_SESSION['newApp']->getMail() == 'mailingBox')) {
+                $this->_f3->reroute('mailing');
+            } elseif(empty($this->_f3->get('errors'))) {
+                $this->_f3->reroute('summary');
+            }
+        }
+
+        //Add years to F3 hive
+        $this->_f3->set('years', Validation::getYears());
+        $this->_f3->set('relocate', OptionalValidation::getLocation());
+        // instantiate a view
+        $view = new Template();
+        echo $view->render("views/experience.html");
     }
 
     function mailing()
     {
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            //instantiate extend class
+            $app = new Applicant_SubscribedToLists();
+
+            if (isset($_POST['software'])) {
+                // move data from POST array to SESSION array
+                $arraySoftware = implode(", ", $_POST['software']);
+                $app->setSelectionsJobs($arraySoftware);
+            }
+            if (isset($_POST['industry'])) {
+                $arrayIndustry = implode(", ", $_POST['industry']);
+                $app->setSelectionsVerticals($arrayIndustry);
+
+            }
+            $_SESSION['app'] = $app;
+            $this->_f3->reroute('summary');
+        }
+
+        //Add array software and industry to the hive
+        $this->_f3->set('software', OptionalValidation::getSelectionsJob());
+        $this->_f3->set('industry', OptionalValidation::getSelectionsVertical());
+
+
+        // instantiate a view
+        $view = new Template();
+        echo $view->render("views/mailing.html");
     }
 
     function summary()
     {
-
+        //instantiate a view
+        $view = new Template(); // template is a fat free class
+        echo $view->render("views/summary.html"); // render method, return text on template
+        //destroy session array
+        session_destroy();
     }
 
 }
--- index.php
+++ index.php
@@ -27,143 +27,26 @@ $f3->route('GET /', function() {
 
 // info.html route (328/job-application/info.html)
 $f3->route('GET|POST /info', function ($f3) {
+    $GLOBALS['control']->info();
 
-    // if the form has been posted
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // instantiate new app
-        $newApp = new Application();
-
-        /// set state
-        $newApp->setState($_POST['state']);
-
-        //trim post names
-        $fname = trim($_POST['fname']);
-        $lname = trim($_POST['lname']);
-        if(Validation::validName($fname) && Validation::validName($lname)) {
-            // set app with post when validated
-            $newApp->setfname($fname);
-            $newApp->setlname($lname);
-
-        } else {
-            $f3->set('errors["name"]',
-                'Name must have at least 2 characters');
-        }
-
-        // email validation
-        $email = trim($_POST['email']);
-        if(Validation::validEmail($email)) {
-            $newApp->setEmail($email);
-        }
-        else $f3->set('errors["email"]',
-            'Email must follow format email@example.com');
-
-
-        // phone validation
-        $phone = trim($_POST['phone']);
-        if(Validation::validPhone($phone))
-        {
-            $newApp->setPhone($phone);
-        }
-        else $f3->set('errors["phone"]',
-            'Phone number must have at least 10 digits');
-
-
-        // checkbox for mailing
-        $mailbox = $_POST['mailingBox'];
-        if (isset($_POST['mailingBox'])){
-            $newApp->setMail($mailbox);
-        }
-
-        // if no errors, then reroute
-        if(empty($f3->get('errors'))) {
-            //mailing session
-            $_SESSION['newApp'] = $newApp;
-            $f3->reroute('experience');
-        }
-    }
-    //instantiate a view
-    $view = new Template(); /// template is a fat free class
-    echo $view->render("views/info.html"); // render method, return text on template
 });
 
 //reroute from info to views/experience.html
 $f3->route('GET|POST /experience', function ($f3) {
+    $GLOBALS['control']->expereince();
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //set and session variables
-        $_SESSION['newApp']->setBio($_POST['bio']);
-
-        $_SESSION['newApp']->setRelocate($_POST['relocate']);
-
-        //validate years selection
-        $years = $_POST['years'];
-        if(Validation::validYears($years)) {
-            $_SESSION['newApp']->setExp($years);
-        }
-        else $f3->set('errors["years"]',
-            'Must Select Years');
-
-        //validate link git hub
-        $link = trim($_POST['link']);
-        if(Validation::validGithub($link)) {
-            $_SESSION['newApp']->setGitHub($link);
-        }
-        else $f3->set('errors["link"]',
-            'Valid URL Format: http://www.example.com');
-
-        //Redirect to mailing page
-        //if there are no errors
-        if (empty($f3->get('errors')) && ($_SESSION['newApp']->getMail() == 'mailingBox')) {
-            $f3->reroute('mailing');
-        } elseif(empty($f3->get('errors'))) {
-            $f3->reroute('summary');
-        }
-    }
-
-    //Add years to F3 hive
-    $f3->set('years', Validation::getYears());
-    $f3->set('relocate', OptionalValidation::getLocation());
-    // instantiate a view
-    $view = new Template();
-    echo $view->render("views/experience.html");
 });
 
 //reroute from experience to views/mailing.html
 $f3->route('GET|POST /mailing', function ($f3) {
+    $GLOBALS['control']->mailing();
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //instantiate extend class
-        $app = new Applicant_SubscribedToLists();
-
-        if (isset($_POST['software']) && ($_POST['industry'])) {
-            // move data from POST array to SESSION array
-            $arraySoftware = implode(", ", $_POST['software']);
-            $arrayIndustry = implode(", ", $_POST['industry']);
-            $app->setSelectionsJobs($arraySoftware);
-            $app->setSelectionsVerticals($arrayIndustry);
-        }
-            $_SESSION['app'] = $app;
-            $f3->reroute('summary');
-    }
-
-    //Add array software and industry to the hive
-    $f3->set('software', OptionalValidation::getSelectionsJob());
-    $f3->set('industry', OptionalValidation::getSelectionsVertical());
-
-
-    // instantiate a view
-    $view = new Template();
-    echo $view->render("views/mailing.html");
 });
 
 // route from mailing and var dump to views/summary.html
 $f3->route('GET /summary', function($f3) {
+    $GLOBALS['control']->summary();
 
-    //instantiate a view
-    $view = new Template(); // template is a fat free class
-    echo $view->render("views/summary.html"); // render method, return text on template
-    //destroy session array
-    session_destroy();
 
 });
 
