0434037a4c203aff6643afe8baeb2fbf4c0b1b44
Zack Dithilath <dithilath.aphixath@greenriver.edu>
2023-03-12 06:12:57 -0400

    construct data layer with insert statement

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,9 +1,14 @@
 <?php
-require_once('/home/zackdith/app-pdo.php'); // connect sql via this file from home cPanel file manager
+require_once($_SERVER['DOCUMENT_ROOT'].'/../app-pdo.php'); // connect sql via this file from home cPanel file manager
 
+/**
+ * This class connects to our database in cPanel
+ */
 class DataLayer
 {
     private $_dbh; // database connection object
+
+    // default constructor that try catch PDO
     function __construct()
     {
         try {
@@ -13,5 +18,19 @@ class DataLayer
         } catch(PDOException $exception) {
             echo $exception->getMessage();
         }
+    }
+
+    /** This functions insert save application into our data base
+     * @param $orderObj
+     * @return void
+     */
+    function saveOrder($orderObj) {
+        // 1. DEFINE QUERY
+        $sql = "INSERT INTO jobs VALUES (null,:name,:link,:phone,:email,:state,:exp,:relocate,:list,:sub)";
+        // 2. PREPARE THE STATEMENT
+        $statement = $this->_dbh->prepare($sql);
+        // 3. BIND THE PARAMETERS
+        $statement->bindParam(':name',
+         );
     }
 }
